da2eb7b7a10dc9c403e1257537f784a41ff5426c
Brennan Caldwell <brennan.caldwell@tqtezos.com>
2021-03-18 08:56:28 -0400

    Remove old sitemap files

--- sidebars.js
+++ sidebars.js
@@ -1,32 +1,4 @@
 module.exports = {
-  sitemap: [
-    'sitemap/home',
-    {
-      type: 'category',
-      label: 'Basics',
-      items: [
-        'sitemap/tezos-overview', 
-        'sitemap/tez'
-      ]
-    },
-    {
-      type: 'category',
-      label: 'Build',
-      items: [
-        'sitemap/get-started', 
-        'sitemap/developer-tools'
-      ]
-    },
-    {
-      type: 'category',
-      label: 'Ecosystem',
-      items: [
-        'sitemap/resources', 
-        'sitemap/events',
-        'sitemap/jobs'
-      ]
-    },
-],
   Learn: [
     'learn/what-is-tezos',
     'learn/store-and-use',
