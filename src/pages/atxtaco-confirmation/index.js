ad633c10a0b585f83aee6b64a4c1c155c8bdead2
Brennan Caldwell <brennan.caldwell@tqtezos.com>
2021-05-04 13:45:53 -0400

    Redirect atxtaco/atxtaco-confirmation pages to discover campaign

--- src/pages/atxtaco-confirmation/index.js
+++ src/pages/atxtaco-confirmation/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 import SignUpConfirm from 'components/campaign/SignUpConfirm'
 
 const AustinTacoConfirm = () => <SignUpConfirm city="Austin" />
 
-export default AustinTacoConfirm
+export default function ATXConfirmRedirect() {
+    return (
+        <Redirect to='/discover' />
+    )
+};
